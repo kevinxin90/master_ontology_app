63c42cbd282e36c90d48e642396fc543b7a18b92
jiwen xin <kevinxin@scripps.edu>
2021-04-10 20:59:26 -0700

    feat: :sparkles: add getOntology lambda function
    get record from Dynamodb based on id

---
+++ backend/src/lambda/http/getOntology.ts
@@ -0,0 +1,31 @@
+import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import 'source-map-support/register'
+import * as AWS from 'aws-sdk'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+const Table = process.env.ONTOLOGIES_TABLE
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    console.log('Processing event: ', event)
+
+    var params = {
+        TableName: Table,
+        Key: {
+            id: event.pathParameters.id
+        }
+    };
+    console.log("pathParameters", event.pathParameters)
+
+    const result = await docClient.get(params).promise();
+
+    return {
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': "*"
+        },
+        body: JSON.stringify({
+            items: result.Item
+        })
+    }
+}
