2ae910911e6789cfb0f77d4b410222fef21a9f2d
jiwen xin <kevinxin@scripps.edu>
2021-04-12 20:22:08 -0700

    feat: :sparkles: sync dynamodb with elasticsearch

--- backend/src/lambda/dynamoDb/elasticSearchSync.ts
+++ backend/src/lambda/dynamoDb/elasticSearchSync.ts
@@ -15,28 +15,103 @@ export const handler: DynamoDBStreamHandler = async (event: DynamoDBStreamEvent)
 
     for (const record of event.Records) {
         console.log('Processing record', JSON.stringify(record))
-        if (record.eventName !== 'INSERT') {
-            continue
+        try {
+            if (record.eventName === 'INSERT') {
+                await insertItem(record)
+            }
+            if (record.eventName === 'REMOVE') {
+                await deleteItem(record)
+            }
+            if (record.eventName === 'MODIFY') {
+                await updateItem(record)
+            }
+        } catch (e) {
+            console.log(e)
         }
-
-        const newItem = record.dynamodb.NewImage
-
-
-        const body = {
-            conceptId: newItem.conceptId.N,
-            description: newItem.description.S,
-            displayName: newItem.displayName.S,
-            parentIds: newItem.parentIds.S,
-            childIds: newItem.childIds.S,
-            alternateNames: newItem.alternateNames.S
-        }
-
-        await es.index({
-            index: 'ontologies-index',
-            type: '_doc',
-            id: newItem.id.S,
-            body
-        })
 
     }
+}
+
+const insertItem = async (record) => {
+    const newItem = record.dynamodb.NewImage;
+    const body = {
+        conceptId: newItem.conceptId.N,
+        description: newItem.description.S,
+        displayName: newItem.displayName.S,
+        parentIds: newItem.parentIds.S,
+        childIds: newItem.childIds.S,
+        alternateNames: newItem.alternateNames.S
+    }
+    const exist = await indexExists();
+    if (!exist) {
+        await es.indices.create("ontologies-index");
+        await createMapping("ontologies-index", "_doc");
+    }
+    await es.index({
+        index: 'ontologies-index',
+        type: '_doc',
+        id: newItem.id.S,
+        body
+    })
+}
+
+const deleteItem = async (record) => {
+    await es.delete({
+        index: 'ontologies-index',
+        type: '_doc',
+        id: record.dynamodb.Keys.id.S,
+    });
+}
+
+const updateItem = async (record) => {
+    await deleteItem(record);
+    await insertItem(record);
+}
+
+const indexExists = async () => {
+    return es.indices.exists({
+        index: 'ontologies-index'
+    })
+}
+
+async function createMapping(index, type) {
+    const schema = {
+        "conceptId": {
+            "type": "long"
+        },
+        "displayName": {
+            "type": "text",
+            "fields": {
+                "keyword": {
+                    "type": "keyword",
+                    "ignore_above": 256
+                }
+            }
+        },
+        "description": {
+            "type": "text",
+            "fields": {
+                "keyword": {
+                    "type": "keyword",
+                    "ignore_above": 256
+                }
+            }
+        },
+        "alternateNames": {
+            "type": "text",
+            "fields": {
+                "keyword": {
+                    "type": "keyword",
+                    "ignore_above": 256
+                }
+            }
+        },
+        "childIds": {
+            "type": "text"
+        },
+        "parentIds": {
+            "type": "text"
+        }
+    }
+    return es.indices.putMapping({ index, type, body: { properties: schema } });
 }
